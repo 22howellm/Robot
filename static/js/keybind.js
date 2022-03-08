ecfbca3e5d6934af5ba1c8539a890e8650dad117
Matthew Howell <Matthew.j.howell@outlook.com>
2022-03-08 14:57:41 +1000

    test 1

--- static/js/keybind.js
+++ static/js/keybind.js
@@ -1,5 +1,22 @@
-function keybindx(){
-    if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.moveAngle = -1; }
-    if (myGameArea.keys && myGameArea.keys[39]) {myGamePiece.moveAngle = 1; }
-    if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speed= 1; }
-    if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speed= -1; }
+document.addEventListener('keydown', function(e){
+    if(!e.repeat){
+        if (e.key === 'w'){
+            console.log('Move forward')
+        }
+        if (e.key === 'a'){
+            console.log('Turn Left')
+        }
+        if (e.key === 'd'){
+            console.log('Turn right')
+        }
+        if (e.key === 's'){
+            console.log('Move backwards')
+        }
+        if (e.key === 'w' && e.shiftKey){
+            console.log('Move forward fast')
+        }
+        if (e.key === 's' && e.shiftKey){
+            console.log('Move backward fast')
+        }
+    }
+})
