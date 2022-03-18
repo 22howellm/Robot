92b65b4f1205618b3f89f2242750855b1001edfc
nielsenb2981 <nielsenb@marash.qld.edu.au>
2022-03-18 12:16:32 +1000

    toggleable speed

--- static/js/keybind.js
+++ static/js/keybind.js
@@ -1,61 +1,65 @@
-if (!(sport)){
-    let sport = true
-    console.log('working')
-}
+
+var sport = true;
 
 document.addEventListener('keydown', function(e){
     if(!e.repeat){
         key = e.key.toLowerCase()
-        if (e.shiftKey){
+        console.log('working4')
+        if (key === 'p'){
             console.log('speed')
             if (sport == false){
-                console.log('true')
-                let sport = true
+                console.log('true');
+                sport = true;
             }
             else if (sport == true) {
-                let sport = false
+                console.log('false');
+                sport = false;
             }
         }
-        else if (key === 'w' && sport === true){
+        if (sport === true){
+            if (key === 'w'){
             console.log('Move forward fast')
             new_ajax_helper('/moveforward');
+            }
+            else if (key === 's'){
+                console.log('Move backward fast')
+                new_ajax_helper('/movebackwards');
+            }
+            else if (key === 'a'){
+                console.log('Turn Left fast')
+                new_ajax_helper('/turnleft');
+            }
+            else if (key === 'd'){
+                console.log('Turn right fast')
+                new_ajax_helper('/turnright');
+            }
         }
-        else if (key === 's' && sport === true){
-            console.log('Move backward fast')
-            new_ajax_helper('/movebackwards');
-        }
-        else if (key === 'a' && sport === true){
-            console.log('Turn Left fast')
-            new_ajax_helper('/turnleft');
-        }
-        else if (key === 'd' && sport === true){
-            console.log('Turn right fast')
-            new_ajax_helper('/turnright');
-        }
-        else if (key === 'd'){
-            console.log('Turn right')
-            new_ajax_helper('/turnrightslow');
-        }
-        else if (key === 'a'){
-            console.log('Turn Left')
-            new_ajax_helper('/turnleftslow');
-        }
-        else if (key === 'w'){
-            console.log('Move forward')
-            new_ajax_helper('/moveforwardslow');
-        }
-        else if (key === 's'){
-            console.log('Move backwards')
-            new_ajax_helper('/movebackwardsslow');
-        }
-        else if (key === 'o'){
-            console.log('shoot')
-            new_ajax_helper('/shoot');
-        }
-        else if (key === 'l'){
-            console.log('shoot')
-            new_ajax_helper('/shoot');
-        }
+        else if (sport === false){
+            if (key === 'd'){
+                console.log('Turn right')
+                new_ajax_helper('/turnrightslow');
+            }
+            else if (key === 'a'){
+                console.log('Turn Left')
+                new_ajax_helper('/turnleftslow');
+            }
+            else if (key === 'w'){
+                console.log('Move forward')
+                new_ajax_helper('/moveforwardslow');
+            }
+            else if (key === 's'){
+                console.log('Move backwards')
+                new_ajax_helper('/movebackwardsslow');
+            }
+            else if (key === 'o'){
+                console.log('shoot')
+                new_ajax_helper('/shoot');
+            }
+            else if (key === 'l'){
+                console.log('shoot')
+                new_ajax_helper('/shoot');
+            }
+    }
     }
 })
 
